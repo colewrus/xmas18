6df38b59d97dce531b93559434cc5c7505d8e5ab
colewrus <wrampelc@gmail.com>
2019-12-24 21:36:57 -0600

    match 3 keychain issue

---
+++ Assets/Scripts/Testing/MatchMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchMgr : MonoBehaviour
+{
+
+    public int xSize;
+    public int ySize;
+    public GameObject matchObject;
+    public Vector3 StartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for(int x=0; x<xSize; x++)
+        {
+            for(int y=0; y<ySize; y++)
+            {
+                Instantiate(matchObject, StartPos + new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
